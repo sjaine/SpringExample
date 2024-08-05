02213ecd78cf67966372ebaeb7e1709251006231
sys <megait@gmail.com>
2024-08-05 20:35:08 +0900

    <Jaine> - Add Initial Proejct

---
+++ src/test/java/design/jaine/spring/ex/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package design.jaine.spring.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
